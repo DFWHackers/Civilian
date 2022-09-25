304ea9fca6d9594b96d8d7a9223e8566a564448f
Shiwei Chen <shiweichenus@gmail.com>
2022-09-25 01:05:14 -0500

    Edit steps

--- client/src/components/stepsPartOne.jsx
+++ client/src/components/stepsPartOne.jsx
@@ -13,12 +13,12 @@ class StepsPartOne extends React.Component {
         <h1 className="recordHeader"> RECORDING... </h1>
         <div>
           <h3>1. Stay in your car </h3>
-          <h3>2. Keep your hands on the steering wheel </h3>
-          <h3>3. Turn on the interior light if it is dark </h3>
+          <h3>2. Keep hands on the steering wheel </h3>
+          <h3>3. Turn on the interior light if it is dark</h3>
           <h3>4. Place the phone in a good spot to film</h3>
-          <h3>5. Politely ask the police the reason of this pullover</h3>
-          <h3>6. Provide the police the documentations required by law</h3>
-          <h3>7. Politely refuse telling the police anything not related</h3>
+          <h3>5. Politely ask police the pulledover reason</h3>
+          <h3>6. Provide the documentations required by law</h3>
+          <h3>7. Politely refuse police anything not related</h3>
           {this.props.clickCallLawyer
             ? < CallLawyer />
             : <button className="callLawyerBtn" onClick={this.props.handleCallLawyer}>CALL MY LAWYER</button>
